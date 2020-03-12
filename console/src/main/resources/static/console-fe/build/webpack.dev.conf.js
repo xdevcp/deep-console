965272f03af7ba371c3b24fb179d4edc35871e78
deep.wu <pan.wu@datavaluable.com>
2020-03-12 16:22:47 +0800

    2

--- console/src/main/resources/static/console-fe/build/webpack.dev.conf.js
+++ console/src/main/resources/static/console-fe/build/webpack.dev.conf.js
@@ -1,3 +1,7 @@
+/*
+ * @Author: deep.ng
+ * @Date: 2020-03-12 14:33:52
+ */
 const path = require('path');
 const webpack = require('webpack');
 const base = require('./webpack.base.conf');
@@ -14,7 +18,7 @@ module.exports = Object.assign({}, base, {
       changeOrigin: true,
       secure: false,
       target: 'http://localhost:8849',
-      pathRewrite: {'^/v1' : '/nacos/v1'}
+      pathRewrite: {'^/v1' : '/app/v1'}
     }],
     disableHostCheck: true,
     open: true,
